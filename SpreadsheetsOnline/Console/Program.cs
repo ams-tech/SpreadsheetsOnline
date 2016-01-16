6bba6d16cdcf7434e039f070521d6560d66b2b4f
ams-tech <ams-tech@users.noreply.github.com>
2016-01-15 19:35:55 -0700

    Added console funciton and console menu

--- SpreadsheetsOnline/Console/Program.cs
+++ SpreadsheetsOnline/Console/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eZet.EveLib.EveCrestModule;
 
 namespace ConsoleApp
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            ConsoleMenu main = new ConsoleMenu("Main Menu");
+
+            main.Execute();
         }
     }
 }
---
+++ SpreadsheetsOnline/SpreadsheetLib/Console/ConsoleFunction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eZet.EveLib;
+
+namespace SpreadsheetLib.Console
+{
+    public class ConsoleFunction
+    {
+        string _menu_text = "Undefined";
+        public delegate void CallbackDelegate();
+
+        CallbackDelegate callback = null;
+
+        public virtual string menu_text
+        {
+            get { return _menu_text; }
+            set { }
+        }
+
+        
+
+        public virtual void Execute()
+        {
+            if (callback == null)
+                throw new NotImplementedException("Callback delegate not defined for this entry");
+            else
+                callback();
+        }
+
+        public ConsoleFunction(string menu_entry_text)
+        {
+            _menu_text = menu_entry_text;
+        }
+
+        public ConsoleFunction(string menu_entry_text, CallbackDelegate callback_delegate)
+        {
+            _menu_text = menu_entry_text;
+            callback = callback_delegate;
+        }
+
+        public ConsoleFunction() { }
+
+    }
+}
